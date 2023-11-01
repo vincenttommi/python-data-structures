c589a5bd4b437a43d3654360e31b8d339c758fb6
vincenttommi <vincenttommi@gmail.com>
2023-11-01 12:30:39 +0300

    accessing lists in python

---
+++ Lists/list.py
@@ -0,0 +1,12 @@
+""""
+
+# The list  is a most  versatile datatype availbale in python  which can be written as a list
+# of comma-separated values btn square  brackets
+
+"""
+
+
+# list1 =  ['physics','chemistry',1997,2023,'tommi']
+
+# for x in list1():
+#     print(list1)
---
+++ accesslist.py
@@ -0,0 +1,5 @@
+vince  = ['physics', 'chemistry','chelsea','2023']
+
+tommi = [1,2,3,4,5,6,7,8,9,10]
+print('vince[0]:',vince[4])
+print('tommi[1:3]:', tommi[1:3])
