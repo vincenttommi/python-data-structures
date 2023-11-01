b68c84520c015a9f9e3c5030f7cdc54f425923cb
vincenttommi <vincenttommi@gmail.com>
2023-11-01 13:13:31 +0300

    acessing tuples

---
+++ Tuples/accesstuples.py
@@ -0,0 +1,20 @@
+""""
+
+
+
+To access values in  tuple , we use square brackets  for slicing along  with the index   or indices 
+to obtain value available at that index
+
+
+
+"""
+
+
+destination = ["kenya",'Las-Vegas','Colombia','London','Moscow','Paris']
+
+ranks  = (1,2,3,4,5,6,7,8,9,10)
+
+print('destination[1]:',destination[1])
+
+print('ranks[5]:',ranks[5])
+
---
+++ Tuples/tuples.py
@@ -0,0 +1,14 @@
+"""
+
+is a sequence of immutable  python Objects
+Tuples cannot  be changed  unlike list
+Tuples use parenthese while list uses square brackets
+
+"""
+
+#Examples of tuples ()
+
+vince  = ('chelsea','Barcelona','Manchester-United','Arsenal')
+
+position  = (11,4,2,8,3)
+
