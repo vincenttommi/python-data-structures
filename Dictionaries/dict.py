450238848258fc56eaef0ffb490aa013524c4551
vincenttommi <vincenttommi@gmail.com>
2023-11-02 11:48:10 +0300

    accesing dictionaries

---
+++ Dictionaries/dict.py
@@ -0,0 +1,27 @@
+"""
+In Dictionary each key is separated by  a colon(:)
+Values of a dictionary can be of any-type
+
+
+"""
+#acessing values in a dictionary
+#To access dictionary  elements  , you can use  the familiar  sqaure brackets along with key to 
+# obtain it's values
+
+#Example
+
+# dict  = {'Name':'vincenttommi','Age':24,'Profession':'programmer'}
+
+
+# print("dict['Name']:", dict['Name'])
+# print("dict['Age']:", dict['Age'])
+# print("dict['Profession']",dict['Profession'])
+
+
+
+players  = {'winger':'Mudryk','RightBack':'Reece james','centerForward':'cole palmer','Goalkeeper':'sanchez'}
+
+print(players['winger'])
+print(players['Goalkeeper'])
+print(players['centerForward'])
+print(players['RightBack'])
