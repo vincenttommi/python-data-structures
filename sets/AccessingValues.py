219f212775dae4e07e4827a7cc7f69e9dd0ff85c
vincenttommi <vincenttommi@gmail.com>
2023-11-03 13:52:54 +0300

    sets in python

---
+++ sets/AccessingValues.py
@@ -0,0 +1,19 @@
+"""
+
+
+
+we cannot  access individual values in a set but we can all access all the elements  together
+
+
+
+
+
+
+
+
+"""
+players = set(['Madueke','Reece James','Cole Palmer','Raheem Sterling','Levi Colwil','Thiago Silva',
+'Mudryk','cucurella','Nkunku Christopher'])
+
+for player in players:
+    print(player)
---
+++ sets/set.py
@@ -0,0 +1,40 @@
+"""
+A Set in Python programming is an unordered collection data type that is iterable, mutable and has no duplicate elements. 
+
+
+is a set of  a collection of items  not in any particular  order.
+Sets are used to store multiple items in a single variable.
+A set is a collection which is unordered, unchangeable*, and unindexed.
+
+
+key points  in sets
+1  Elements  in the set cannot  be duplicates
+2  Elements in sets are immutable
+3 There is no index  attached  to any element in a python set.
+
+
+Set Operation
+1 sets are typical used for mathematical Operationslike union,intersection,difference and  complement
+
+
+
+creating a set 
+A set is created using the  set() function or  placing all elements  within a pair  of curly brakets
+
+
+
+
+When the above code is executed, it produces the following result. Please note how the order of the elements has changed in the result
+"""
+#Example of a set
+
+
+clubs = set(['Chelsea','Manchester-United','Man-city','Liverpool','Totehnam','NewCastle'])
+captains  = {"Reece James","Bruno Fernandes","Kevin-Debruyne","Vigil",'Son','Trippier'}
+
+position  = {10,8,2,3,1,5}
+
+print(clubs)
+print(captains)
+print(position)
+
